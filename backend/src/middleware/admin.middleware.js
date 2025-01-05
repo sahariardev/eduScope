275fc5d8137ddf2ef2c7d236d630886d4d0f1f2b
sahariardev <rifatsahariar@gmail.com>
2025-01-05 12:26:02 +0600

    Converted to nest js

--- backend/src/middleware/admin.middleware.js
+++
@@ -1,9 +0,0 @@
-export const adminChecker = (req, res, next) => {
-    if (!req.user.isAdmin) {
-        return res.status(401).json({
-            message: 'Unauthorized access'
-        });
-    }
-
-    next();
-}
--- backend/src/routes/auth.router.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {login, signUp} from "../controllers/auth.controller.js";
-
-const authRouter = new Router();
-
-authRouter.post('/signup', signUp);
-authRouter.post('/login', login);
-
-export default authRouter;
