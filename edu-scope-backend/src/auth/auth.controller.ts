fb0c06d7565e5770b7f37b47faf7724d9d7b0bcc
sahariardev <rifatsahariar@gmail.com>
2025-01-09 22:35:27 +0600

    Added gurd to protect routes

--- edu-scope-backend/src/auth/auth.controller.ts
+++ edu-scope-backend/src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, UseGuards, Request} from '@nestjs/common';
 import {AuthService} from "./auth.service";
 import {AuthDto} from "./dto";
+import {AuthGuard} from "./auth.gurd";
+import {Public} from "../app.utils";
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {
     }
 
+    @Public()
     @Post('signup')
     signup(@Body() dto: AuthDto) {
         return this.authService.signup(dto);
     }
 
+    @Public()
     @Post('signin')
     signin(@Body() dto: AuthDto) {
         return this.authService.signin(dto);
--- edu-scope-backend/src/auth/auth.module.ts
+++ edu-scope-backend/src/auth/auth.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import {Module} from '@nestjs/common';
+import {AuthController} from './auth.controller';
+import {AuthService} from './auth.service';
 import {JwtModule} from "@nestjs/jwt";
+import {AuthGuard} from "./auth.gurd";
+import {APP_GUARD} from "@nestjs/core";
 
 @Module({
-  imports: [JwtModule.register({
-    
-  })],
-  controllers: [AuthController],
-  providers: [AuthService],
+    imports: [JwtModule.register({})],
+    controllers: [AuthController],
+    providers: [
+        AuthService,
+        {
+            provide: APP_GUARD,
+            useClass: AuthGuard,
+        },
+    ],
 })
-export class AuthModule {}
+export class AuthModule {
+}
